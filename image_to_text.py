ae01af353b7b35bacf8761ab0d5e741b55dcc9f9
KARIM ABOELAZM <74789405+karim-aboelazm@users.noreply.github.com>
2022-05-29 11:42:10 +0200

    Delete image_to_text.py

--- image_to_text.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
-im = Image.open("omg.png")
-
-text = pytesseract.image_to_string(im, lang = 'eng')
-
-print(text)
